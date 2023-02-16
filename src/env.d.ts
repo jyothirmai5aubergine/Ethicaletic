b08682450051bdd674accb8cf5c511aeec6a7721
jyothirmai5aubergine <125560744+jyothirmai5aubergine@users.noreply.github.com>
2023-02-16 16:46:23 +0530

    Add files via upload

---
+++ src/env.d.ts
@@ -0,0 +1,13 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_APP_TITLE: string
+    readonly VITE_APP_ENV:string
+    readonly VITE_APP_PRODUCTION:boolean
+    readonly VITE_APP_BASE_URL:string
+    // more env variables...
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv
+  }
