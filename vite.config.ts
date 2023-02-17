4f2e05fe082110733294ebb74388b92dc72d633d
NishaSataki <nisha@auberginesolutions.com>
2023-02-17 18:04:54 +0530

    Set env and gitignore files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+import react from "@vitejs/plugin-react";
+
+export default () => {
+
+  return defineConfig({
+    plugins: [react(), tsconfigPaths()],
+    build: {
+      chunkSizeWarningLimit: 1600,
+    },
+  });
+};
